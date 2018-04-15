46e473c779cd0a61ae06da3d13dc92b9d9e4409a
Ryan Schroeder <schroeder2013@gmail.com>
2018-04-15 18:56:07 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Ryan Schroeder on 4/15/18.
+//  Copyright © 2018 Ryan Schroeder. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
